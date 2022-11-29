2e5e951cce8f8583c66318318033393c75d377c4
valeroman <valeraromanovo@gmail.com>
2022-11-29 18:03:05 +0300

    Fix switch border button

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
-let sketchWidth = 8;
+let sketchWidth = 12;
 let dotColor = 'red';
+let dotBorderWidth = 1;
 
 const body = document.querySelector('body');
 
@@ -7,27 +8,50 @@ const mainContainer = document.createElement('div');
 mainContainer.setAttribute('class', 'main-container');
 body.appendChild(mainContainer);
 
+const controlPanel = document.createElement('div');
+mainContainer.setAttribute('class', 'control-panel');
+mainContainer.appendChild(controlPanel);
+
 const widthButton = document.createElement('button');
 widthButton.textContent = 'Set width';
-mainContainer.appendChild(widthButton);
+controlPanel.appendChild(widthButton);
+
+const switchBorderButton = document.createElement('button');
+switchBorderButton.textContent = 'Switch border';
+controlPanel.appendChild(switchBorderButton);
 
 const sketchPad = document.createElement('div');
 sketchPad.setAttribute('class', 'sketchpad');
 mainContainer.appendChild(sketchPad);
 
-createDivGrid(sketchPad, sketchWidth, 1);
+createDivGrid(sketchPad, sketchWidth, dotBorderWidth);
+
 
 widthButton.onclick = () => {
-    const sketchWidth = +prompt("input width number", "16");
-    if (!sketchWidth) return;
+    const input = +prompt("input width number", "16");
+    if (!input) return;
+    sketchWidth = input;  
     deleteGrid(sketchPad);
-    createDivGrid(sketchPad, sketchWidth, 1);
+    createDivGrid(sketchPad, sketchWidth, dotBorderWidth);
 }
+
+switchBorderButton.onclick = () => switchBorder();
 
 sketchPad.onmousedown = () => fillDots(dotColor);
 sketchPad.ontouchstart = () => fillDots(dotColor);
 sketchPad.onmouseup = () => stopFillDots(dotColor);
 sketchPad.ontouchend = () => stopFillDots(dotColor);
+
+
+function switchBorder() {
+    if (dotBorderWidth === 0) dotBorderWidth = 1;
+    else dotBorderWidth = 0;
+    const dots = document.getElementsByClassName('dot');
+    for (let dot of dots) {
+        dot.style.borderWidth = dotBorderWidth + 'px';
+        setPadding(sketchPad, dot);
+    }
+}
 
 function deleteGrid(parentElem) {
     parentElem.innerHTML = '';
@@ -54,27 +78,35 @@ function changeBGC(elem, color) {
     elem.style.backgroundColor = color;
 }
 
-function setGrid(parentElem, widthNum) {
+function setGrid(parentElem, sketchWidth) {
     parentElem.style.display = 'grid';
     parentElem.style.gridTemplateColumns = '';
-    for (let i = 0; i < widthNum; i++) {
+    for (let i = 0; i < sketchWidth; i++) {
         parentElem.style.gridTemplateColumns += ' auto';
     }
 }
 
-function createDivGrid(parentElem, widthNum, borderWidth) {
-    setGrid(parentElem, widthNum);
-    const windowWidth = window.innerWidth;
-    const parentMaxWidth = parseInt(window.getComputedStyle(parentElem).maxWidth);
-    for (let i = 1; i <= widthNum * widthNum; i++) {
+function createDivGrid(parentElem, sketchWidth, dotBorderWidth) {
+    setGrid(parentElem, sketchWidth);
+    for (let i = 1; i <= sketchWidth * sketchWidth; i++) {
         let dotDiv = document.createElement('div');
         dotDiv.setAttribute('class', 'dot');
-        dotDiv.style.border = borderWidth + 'px solid grey';
-        if (windowWidth < parentMaxWidth) {
-            dotDiv.style.padding = (windowWidth / widthNum / 2 - borderWidth * 2) + 'px';
-        } else { 
-            dotDiv.style.padding = (parentMaxWidth / widthNum / 2 - borderWidth * 2) + 'px';
-        }
+        dotDiv.style.border = dotBorderWidth + 'px solid lightgrey';
+        setPadding(parentElem, dotDiv);
         parentElem.appendChild(dotDiv);
+    }
+}
+
+function countPadding(viewWidth, borderWidth, sketchWidth) {
+    return viewWidth / sketchWidth / 2 - borderWidth  + 'px';
+}
+
+function setPadding(parentElem, elem) {
+    const windowWidth = window.innerWidth;
+    const parentMaxWidth = parseInt(window.getComputedStyle(parentElem).maxWidth);
+    if (windowWidth < parentMaxWidth) {
+        elem.style.padding = countPadding(windowWidth, dotBorderWidth, sketchWidth);
+    } else { 
+        elem.style.padding = countPadding(parentMaxWidth, dotBorderWidth, sketchWidth);
     }
 }
